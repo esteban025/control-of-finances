ee9e7b279270c9a46497d0cee675ffe3982e24c4
EstebanDev <guachichullcaesteban@gmail.com>
2025-06-04 18:44:49 -0500

    Agregar archivo nav.ts con elementos de navegación y actualizar tsconfig.json para incluir la ruta de acceso a datos

---
+++ src/data/nav.ts
@@ -0,0 +1,12 @@
+interface NavItem {
+  name: string;
+  href: string;
+}
+const navItems: NavItem[] = [
+  { name: 'beneficios', href: '#benefits' },
+  { name: 'como funciona', href: '#how-it-works' },
+  { name: 'consejos', href: '#advices' },
+  { name: 'FAQ', href: '#faq' },
+];
+
+export default navItems;
