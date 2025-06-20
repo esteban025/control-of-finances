e773b7d1defe1e7efac9173a47d92d6cbacff19b
EstebanDev <guachichullcaesteban@gmail.com>
2025-06-20 08:46:33 -0500

    chore: update dependencies and add new packages
    - Added @astrojs/db and @astrojs/netlify for improved Astro integration.
    - Included @auth/core and auth-astro for enhanced authentication support.
    - Added zustand for state management.

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,11 +5,19 @@ import tailwindcss from '@tailwindcss/vite';
 
 import react from '@astrojs/react';
 
+import db from '@astrojs/db';
+
+import auth from 'auth-astro';
+
+import netlify from '@astrojs/netlify';
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
   },
 
-  integrations: [react()]
+  integrations: [react(), db(), auth()],
+  adapter: netlify(),
+  output: "server"
 });
